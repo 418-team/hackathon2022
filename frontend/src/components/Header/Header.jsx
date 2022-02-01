a64cc307fa547e57ecc5264f235a79b04ef49890
Ruslan Dinullin <dinullin2013@yandex.ru>
2022-02-01 23:28:14 +0500

    Start header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import "./header.css";
 
+import { useState } from "react";
 import { useHistory } from "react-router-dom";
 
 import Logo from "./Logo";
 
 function Header() {
   const history = useHistory();
+  const [activeMenu, setActiveMenu] = useState(false);
 
   const logout = () => {
     localStorage.clear();
@@ -13,26 +15,43 @@ function Header() {
   };
 
   return (
-    <div className="header-nav">
-      <div className="header-section">
-        <span className="header-item" onClick={() => history.push("/")}>
-          <Logo />
-        </span>
+    <>
+      <div className="header-nav">
+        <div className="header-section">
+          <span className="header-item" onClick={() => history.push("/")}>
+            <Logo />
+          </span>
+        </div>
+        <div className="header-section">
+          <span
+            className="header-item"
+            onClick={() => history.push("/cabinet")}
+          >
+            Личный кабинет
+          </span>
+          <span className="header-item" onClick={() => history.push("/team")}>
+            Команда
+          </span>
+        </div>
+        <div className="header-section">
+          <span className="header-item" onClick={logout}>
+            Выйти
+          </span>
+        </div>
       </div>
-      <div className="header-section">
-        <span className="header-item" onClick={() => history.push("/cabinet")}>
-          Личный кабинет
-        </span>
-        <span className="header-item" onClick={() => history.push("/team")}>
-          Команда
-        </span>
+      <div className="mobile_header mobile_header-active">
+        <div className="mobile_header__section">
+          <span className="section_item" onClick={() => history.push("/")}>
+            <Logo fill={activeMenu ? 'black' : 'white'} />
+          </span>
+        </div>
+        <div className="mobile_header__section">
+          <span className="section_item mobile_header__dots">
+            {activeMenu ? '🗙' : '⏺ ⏺'}
+          </span>
+        </div>
       </div>
-      <div className="header-section">
-        <span className="header-item" onClick={logout}>
-          Выйти
-        </span>
-      </div>
-    </div>
+    </>
   );
 }
 
